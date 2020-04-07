e5e211af6957a5359425c02a96c4407ed42caae3
hyeongpil <hyeongpil@lotte.net>
2020-04-07 17:04:21 +0900

    nuxt-property-decorator로 변경

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component } from 'nuxt-property-decorator'
 import TheNavbarHorizontal from '~/components/UI/navbar/TheNavbarHorizontal.vue'
 import TheFooter from '~/components/UI/footer/TheFooter.vue'
 import { globalStore } from '@/store'
